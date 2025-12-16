9c1eba2f8986d97e43ff2cb759bf2c87bcf1e8ed
steppi <1953382+steppi@users.noreply.github.com>
2025-12-16 18:16:15 -0500

    Add script to retrain all models

---
+++ scripts/retrain_all_models.py
@@ -0,0 +1,59 @@
+import argparse
+import logging
+import multiprocessing
+
+import adeft
+
+from adeft.locations import ADEFT_PATH
+from adeft.util import get_canonical_model_name
+
+from adeft_indra.results import ResultsManager
+from adeft_indra.training import get_existing_grounding_info
+from adeft_indra.training import validate_and_refit_model
+
+logger = logging.getLogger(__file__)
+
+
+def retrain_model_worker(shortform, results_db_path):
+    results_manager = ResultsManager(results_db_path)
+    grounding_dict, names, pos_labels = get_existing_grounding_info(shortform)
+    model_name = get_canonical_model_name(grounding_dict.keys())
+
+    if model_name in results_manager:
+        return
+
+    logger.info(f"Retraining {model_name}.")
+    model = validate_and_refit_model(
+        grounding_dict,
+        names,
+        pos_labels,
+        random_state=1729,
+    )
+    logger.info(f"{model_name} finished retraining.")
+    results_manager[model_name] = model
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("results_db_path")
+    parser.add_argument("--n_jobs", type=int, default=1)
+
+    args = parser.parse_args()
+    results_manager = ResultsManager(args.results_db_path)
+    
+    models_dict = adeft.get_available_models()
+    # Exclude models which have already been retrained.
+    reduced_shortforms = list(
+        {
+            val: key for key, val in models_dict.items() if val not in results_manager
+        }.values()
+    )
+
+    with multiprocessing.Pool(args.n_jobs) as pool:
+        pool.starmap(
+            retrain_model_worker,
+            [
+                (shortform, args.results_db_path)
+                for shortform in reduced_shortforms
+            ]
+        )
